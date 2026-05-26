9d464f23cadd1d9320a79ce81131f0184c9e0caf
Starwort <tcphone93@gmail.com>
2026-05-26 14:00:06 +0100

    Add deep link support to the launcher component

--- apworld/components.py
+++ apworld/components.py
@@ -4,8 +4,25 @@ from worlds.LauncherComponents import Component, Type, components, icon_paths
 
 
 def run_client(*args: str) -> None:
-    webbrowser.open("https://starwort.github.io/suikapelago/")
+    from urllib.parse import urlencode
 
+    from CommonClient import get_base_parser, handle_url_arg
+
+    parser = get_base_parser()
+    parser.add_argument("--name", default=None, help="Slot Name to connect as.")
+    parser.add_argument("url", nargs="?", help="Archipelago connection url")
+
+    launch_args = handle_url_arg(parser.parse_args(args))
+
+    query = ""
+    if launch_args.name and launch_args.connect:
+        query = "?" + urlencode({
+            "player": launch_args.name,
+            "server": launch_args.connect,
+            "password": launch_args.password,
+        })
+
+    webbrowser.open("https://starwort.github.io/suikapelago/" + query)
 
 icon_paths["Suikapelago"] = "ap:worlds.suikapelago/icon.png"
 
